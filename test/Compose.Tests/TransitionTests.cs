fb366ec4afab41db24c22af0ee0a1fe783c60150
Tommy Long <tommy.long@4com.co.uk>
2015-02-26 16:06:44 +0000

    Basic transitional test cases added

---
+++ test/Compose.Tests/TransitionTests.cs
@@ -0,0 +1,104 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Compose.Tests
+{
+	public class TransitionTests
+	{
+		[Fact]
+		public void CanResolveServiceAddedAsTransitional()
+		{
+			var app = new TestApplication();
+			app.UseServices(services => { services.AddTransitional<IDependency, Dependency>(); });
+			app.OnExecute<IDependency>(dependency =>
+			{
+				dependency.Should().NotBeNull();
+				dependency.Should().BeOfType<Dependency>();
+			});
+			app.Execute();
+		}
+
+		[Fact]
+		public void CanResolveDirectServiceAddedAsTransitional()
+		{
+			var app = new TestApplication();
+			app.UseServices(services => { services.AddTransitional<IDependency, DirectDependency>(); });
+			app.OnExecute<IDependency>(dependency =>
+			{
+				dependency.Should().NotBeNull();
+				dependency.Should().BeOfType<DirectDependency>();
+			});
+			app.Execute();
+		}
+
+		[Fact]
+		public void CanResolveFactoryForServiceAddedAsTransitional()
+		{
+			var app = new TestApplication();
+			app.UseServices(services => { services.AddTransitional<IDependency, Dependency>(); });
+			app.OnExecute<IFactory<IDependency>>(factory =>
+			{
+				factory.Should().NotBeNull();
+				factory.GetService().Should().NotBeNull();
+				factory.GetService().Should().BeOfType<Dependency>();
+			});
+			app.Execute();
+		}
+
+		[Fact]
+		public void CanTransitionUndirectService()
+		{
+			var app = new TestApplication();
+			app.UseServices(services => { services.AddTransitional<IDependency, Dependency>(); });
+			app.OnExecute<IDependency>(dependency =>
+			{
+				app.Transition<IDependency, OtherDependency>();
+				dependency.Should().BeOfType<Dependency>();
+			});
+		}
+
+		[Fact]
+		public void CanTransitionDirectService()
+		{
+			var app = new TestApplication();
+			app.UseServices(services => { services.AddTransitional<IDependency, DirectDependency>(); });
+			app.OnExecute<IDependency>(dependency =>
+			{
+				dependency.Should().BeOfType<DirectDependency>();
+				dependency.Id.Should().Be(Type.Dependency);
+				app.Transition<IDependency, OtherDependency>();
+				dependency.Id.Should().Be(Type.OtherDependency);
+			});
+		}
+
+		[Fact]
+		public void CanTransitionFactoryService()
+		{
+			var app = new TestApplication();
+			app.UseServices(services => { services.AddTransitional<IDependency, Dependency>(); });
+			app.OnExecute<IFactory<IDependency>>(factory =>
+			{
+				factory.GetService().Id.Should().Be(Type.Dependency);
+				app.Transition<IDependency, OtherDependency>();
+				factory.GetService().Id.Should().Be(Type.OtherDependency);
+			});
+			app.Execute();
+		}
+
+		private enum Type { Dependency, OtherDependency, DirectDependency }
+
+		private interface IDependency { Type Id { get; } }
+
+		private class Dependency : IDependency { public Type Id { get; private set; } = Type.Dependency; }
+
+		private class OtherDependency : IDependency { public Type Id { get; private set; } = Type.OtherDependency; }
+
+		private class DirectDependency : DirectTransition<IDependency, Dependency>, IDependency
+		{
+			public Type Id { get { return Service.Id; } }
+			public DirectDependency(Dependency dependency) : base(dependency) { }
+		}
+
+		private class TestApplication : Executable { internal void Execute() { Execution(); } }
+	}
+}
