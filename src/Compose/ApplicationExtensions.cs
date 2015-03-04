487f263dc01db9395d068109ae853b6fc2ded036
Smudge202 <pete_201@hotmail.com>
2015-03-04 21:15:34 +0000

    Dynamic Helpers moved to Application extensions as per @mattridgway suggestion

--- src/Compose/ApplicationExtensions.cs
+++ src/Compose/ApplicationExtensions.cs
@@ -28,6 +28,25 @@ namespace Compose
 			var transitional = app.HostingServices.GetService<TService>() as ITransition<TService>;
 			if (transitional == null) throw new InvalidOperationException($"{typeof(TService).Name} must be registered as a Transitional Service (services.AddTransitional<{typeof(TService).Name}, TImplementation>()");
 			return transitional.Change(app.GetRequiredService<TImplementation>());
-		}
-	}
+        }
+
+        internal static Type CreateProxy(this Application app, Type service)
+        {
+            var emitter = app.HostingServices.GetService<DynamicEmitter>();
+            if (emitter == null) emitter = app.GetRegisteredDynamicEmitter();
+
+            return emitter.GetDirectTransitionImplementation(service);
+        }
+
+        internal static TService CreateProxy<TService>(this Application app) where TService : class
+        {
+            return (TService)Activator.CreateInstance(app.CreateProxy(typeof(TService)), app.GetRequiredService<TService>());
+        }
+
+        private static DynamicEmitter GetRegisteredDynamicEmitter(this Application app)
+        {
+            app.Provider = app.Provider.Extend(new ServiceDescriptor(typeof(DynamicEmitter), typeof(DynamicEmitter), LifecycleKind.Singleton));
+            return app.GetRequiredService<DynamicEmitter>();
+        }
+    }
 }
