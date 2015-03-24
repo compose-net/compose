e11d7b6b74109f14cd5aa318d62a8fa48af526e9
Tommy Long <tommy.long@4com.co.uk>
2015-03-24 09:53:49 +0000

    Test confirming issue #27

--- tests/Compose.Tests/Fake/Application.cs
+++ tests/Compose.Tests/Fake/Application.cs
@@ -1,4 +1,12 @@
 ﻿namespace Compose.Tests.Fake
 {
-	internal class Application : Executable { }
+	internal class Application : Executable
+	{
+		public override void Execute()
+		{
+			if (Execution == null)
+				OnExecute(() => { });
+			base.Execute();
+		}
+	}
 }
