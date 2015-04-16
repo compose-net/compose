9afdf99082073c0d07d6e3c8d1e77b92c1d8abba
Tommy Long <tommy.long@4com.co.uk>
2015-04-16 11:18:46 +0100

    Allowed specifying return type on CommandLineApplication Minor increment

--- src/Compose/CommandLineApplication.cs
+++ src/Compose/CommandLineApplication.cs
@@ -2,6 +2,7 @@
 
 namespace Compose
 {
-	public class CommandLineApplication : Executable<int> { }
+	public class CommandLineApplication : CommandLineApplciation<int> { }
+	public class CommandLineApplciation<TResult> : Executable<TResult> { }
 	public class CancellableCommandLineApplication : Executable<CancellationToken, int> { }
 }
