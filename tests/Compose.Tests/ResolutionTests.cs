edc8deb748a233a99549d72994e009f13fdc745c
Tommy Long <tommy.long@4com.co.uk>
2015-05-20 09:00:04 +0100

    Resolves #60

---
+++ tests/Compose.Tests/ResolutionTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Microsoft.Framework.DependencyInjection;
+using System;
+using Xunit;
+
+namespace Compose.Tests
+{
+	public class ResolutionTests
+	{
+		private interface IDependency { }
+
+		private interface IDependency2 { }
+
+		private class Dependency : IDependency { }
+
+		[Fact]
+		public void WhenResolvingUnboundServiceThenThrowsDescriptiveException()
+		{
+			var app = new Application();
+
+			app.UseServices(services => services.Add(ServiceDescriptor.Transient<IDependency, Dependency>()));
+
+			Action act = () => app.GetRequiredService<IDependency2>();
+			act.ShouldThrow<InvalidOperationException>();
+		}
+	}
+}
