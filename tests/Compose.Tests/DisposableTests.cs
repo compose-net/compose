08b3f3d3e5044ca7db814f41703cb48eacb346dd
Tommy Long <tommy.long@4com.co.uk>
2015-06-02 14:45:18 +0100

    Fixes smudge202/compose#68

---
+++ tests/Compose.Tests/DisposableTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Framework.DependencyInjection;
+using System;
+using Xunit;
+
+namespace Compose.Tests
+{
+    public class DisposableTests
+    {
+		[Fact]
+		public void WhenTransitioningAwayFromDirectlyImplementedDisposableThenDisposesCurrentService()
+		{
+			var app = new Fake.Application();
+			app.UseServices(services => services
+				.AddTransitional<IDependency, DirectlyDisposableDependency>()
+				.AddTransient<Dependency, Dependency>()
+			);
+			app.OnExecute<IDependency>(dependency =>
+			{
+				Action act = app.Transition<IDependency, Dependency>;
+				Assert.Throws<NotImplementedException>(act);
+            });
+		}
+
+		[Fact]
+		public void WhenTransitioningAwayFromIndirectlyImplementedDisposableThenDisposesCurrentService()
+		{
+			var app = new Fake.Application();
+			app.UseServices(services => services
+				.AddTransitional<IDependency, IndirectlyDisposableDependency>()
+				.AddTransient<Dependency, Dependency>()
+			);
+			app.OnExecute<IDependency>(dependency =>
+			{
+				Action act = app.Transition<IDependency, Dependency>;
+				Assert.Throws<NotImplementedException>(act);
+			});
+		}
+
+		internal interface IDependency { }
+
+		private class Dependency : IDependency { }
+
+		private class DirectlyDisposableDependency : IDependency, IDisposable
+		{
+			public void Dispose() { throw new NotImplementedException(); }
+		}
+
+		private interface IDisposableDependency : IDependency, IDisposable { }
+
+		private class IndirectlyDisposableDependency : IDisposableDependency
+		{
+			public void Dispose() { throw new NotImplementedException(); }
+		}
+	}
+}
