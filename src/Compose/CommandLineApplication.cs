2d623c7755136dd25e80747157b15783290fa997
herecydev <herecy@live.co.uk>
2015-07-08 09:00:26 +0100

    Fixed tabs

--- src/Compose/CommandLineApplication.cs
+++ src/Compose/CommandLineApplication.cs
@@ -1,6 +1,6 @@
 ﻿namespace Compose
 {
 	public class CommandLineApplication : CommandLineApplication<int> { }
-    public class CommandLineApplication<TResult> : Executable<TResult> { }
+	public class CommandLineApplication<TResult> : Executable<TResult> { }
 	public class CommandLineApplication<TContext, TResult> : Executable<TContext, TResult> { }
 }
