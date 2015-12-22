cb4db3598a5c86b0f832e8a9b1c69d7006e7528b
Stuart Blackler <stuart@sblackler.net>
2015-12-22 23:21:22 +0000

    [WIP] Fast activation

---
+++ src/Compose/Activate.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Compose
+{
+    public sealed class Activate
+    {
+        private static readonly object[] _emptyObjectArray = new object[0];
+        private static ConcurrentDictionary<TypeInfo, TypeCacheEntry> _typeCache = new ConcurrentDictionary<TypeInfo, TypeCacheEntry>();
+
+        public static object Type(TypeInfo serviceType) => Type(serviceType, _emptyObjectArray);
+        public static object Type(TypeInfo serviceType, object arg1) => Type(serviceType, new[] { arg1 });
+        public static object Type(TypeInfo serviceType, object arg1, object arg2) => Type(serviceType, new[] { arg1, arg2 });
+        public static object Type(TypeInfo serviceType, object arg1, object arg2, object arg3) => Type(serviceType, new[] { arg1, arg2, arg3 });
+        public static object Type(TypeInfo serviceType, object arg1, object arg2, object arg3, object arg4) => Type(serviceType, new[] { arg1, arg2, arg3, arg4 });
+        public static object Type(TypeInfo serviceType, object[] args)
+        {
+            if (serviceType == null)
+                throw new ArgumentNullException(nameof(serviceType));
+
+            if (serviceType.IsInterface || serviceType.IsAbstract)
+                throw new ArgumentException("You cannot activate a type that is marked as an interface or abstract");
+
+            return _typeCache.GetOrAdd(serviceType, type => new TypeCacheEntry(type)).Create(ref args);
+        }
+
+        private class TypeCacheEntry
+        {
+            private Func<object> _fastCreator = null;
+            private Ctor[] _constructors;
+
+            internal bool HasParameterlessCtor => _fastCreator != null;
+
+            internal TypeCacheEntry(TypeInfo type)
+            {
+                _constructors = type.DeclaredConstructors.Select(x => new Ctor(x)).ToArray();
+
+                var fastCtor = _constructors.FirstOrDefault(x => x.ArgumentCount == 0).ConstructorInfo;
+                if(fastCtor != null)
+                    _fastCreator = Expression.Lambda<Func<object>>(Expression.New(fastCtor)).Compile();
+            }
+
+            internal object Create(ref object[] args)
+            {
+                if (args.Length == 0 && HasParameterlessCtor)
+                    return _fastCreator();
+
+                for(var i = 0; i < _constructors.Length; i++)
+                {
+                    var ctor = _constructors[i];
+                    if (ctor.IsMatch(ref args))
+                        return ctor.Invoke(ref args);
+                }
+
+                throw new TypeInitializationException("", null);
+            }
+        }
+
+        private class Ctor
+        {
+            private readonly Func<object[], object> _creator;
+            private TypeInfo[] _typeArguments;
+
+            internal int ArgumentCount => _typeArguments.Length;
+            internal ConstructorInfo ConstructorInfo { get; }
+
+            internal Ctor(ConstructorInfo ctorInfo)
+            {
+                ConstructorInfo = ctorInfo;
+                _typeArguments = ctorInfo.GetParameters().Select(x => x.ParameterType.GetTypeInfo()).ToArray();
+
+                var array = Expression.Parameter(typeof(object[]));
+                var parameters = new List<Expression>();
+                for(var i = 0; i < ArgumentCount; i++)
+                {
+                    parameters.Add(Expression.ConvertChecked(Expression.ArrayIndex(array, Expression.Constant(i)), _typeArguments[i].AsType()));
+                }
+
+                _creator = Expression.Lambda<Func<object[], object>>(Expression.New(ctorInfo, parameters), array).Compile();
+            }
+
+            internal bool IsMatch(ref object[] args)
+            {
+                if (args.Length != ArgumentCount)
+                    return false;
+
+                for(var i = 0; i < ArgumentCount; i++)
+                    if(!args[i].GetType().GetTypeInfo().IsAssignableFrom(_typeArguments[i]))
+                        return false;
+
+                return true;
+            }
+            internal object Invoke(ref object[] args) => _creator(args);            
+        }
+    }
+}
---
+++ tests/Compose.Tests/ActivateTests.cs
@@ -0,0 +1,80 @@
+﻿using Compose;
+using FluentAssertions;
+using System.Reflection;
+using Xunit;
+
+namespace Compose.Tests
+{
+    public class ActivateTests
+    {
+        public class GivenAPublicClass
+        {
+            public class PublicClass
+            { }
+
+            public class PublicClassWithPrivateConstructors
+            {
+                private PublicClassWithPrivateConstructors() { }
+                private PublicClassWithPrivateConstructors(int a) { }
+                private PublicClassWithPrivateConstructors(string a) { }
+            }
+
+            [Fact]
+            public void WithNoConstructorArgumentsThenReturnsObjectOfType()
+            {
+                var result = Activate.Type(typeof(PublicClass).GetTypeInfo());
+
+                result.Should().NotBeNull();
+                result.Should().BeAssignableTo<PublicClass>();
+            }
+
+            [Fact]
+            public void WithPrivateConstructorThenReturnsObjectOfType()
+            {
+                var result = Activate.Type(typeof(PublicClassWithPrivateConstructors).GetTypeInfo());
+
+                result.Should().NotBeNull();
+                result.Should().BeAssignableTo<PublicClassWithPrivateConstructors>();
+            }
+
+            [Fact]
+            public void WithPrivateConstructorWhenPassedArgumentsThenReturnsObjectOfType()
+            {
+                var result = Activate.Type(typeof(PublicClassWithPrivateConstructors).GetTypeInfo(), 1);
+
+                result.Should().NotBeNull();
+                result.Should().BeAssignableTo<PublicClassWithPrivateConstructors>();
+            }
+        }
+
+        public class GivenAnInternalClass
+        {
+            internal class InternalClass
+            { }
+
+            [Fact]
+            public void WithNoConstructorArgumentsThenReturnsObjectOfType()
+            {
+                var result = Activate.Type(typeof(InternalClass).GetTypeInfo());
+
+                result.Should().NotBeNull();
+                result.Should().BeAssignableTo<InternalClass>();
+            }
+        }
+
+        public class GivenAPrivateClass
+        {
+            private class PrivateClass
+            { }
+
+            [Fact]
+            public void WithNoConstructorArgumentsThenReturnsObjectOfType()
+            {
+                var result = Activate.Type(typeof(PrivateClass).GetTypeInfo());
+
+                result.Should().NotBeNull();
+                result.Should().BeAssignableTo<PrivateClass>();
+            }
+        }
+    }
+}
