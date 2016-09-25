17fc0db21e3c1ff72b374aed458d29104b373cea
Daniel Cirket <daniel.cirket@gmail.com>
2016-09-25 10:26:01 +0100

    cleaned  up build script

--- build.ps1
+++ build.ps1
@@ -1,29 +1,3 @@
-<#  
-.SYNOPSIS
-    You can add this to you build script to ensure that psbuild is available before calling
-    Invoke-MSBuild. If psbuild is not available locally it will be downloaded automatically.
-#>
-function EnsurePsbuildInstalled{  
-    [cmdletbinding()]
-    param(
-        [string]$psbuildInstallUri = 'https://raw.githubusercontent.com/ligershark/psbuild/master/src/GetPSBuild.ps1'
-    )
-    process{
-        if(-not (Get-Command "Invoke-MsBuild" -errorAction SilentlyContinue)){
-            'Installing psbuild from [{0}]' -f $psbuildInstallUri | Write-Verbose
-            (new-object Net.WebClient).DownloadString($psbuildInstallUri) | iex
-        }
-        else{
-            'psbuild already loaded, skipping download' | Write-Verbose
-        }
-
-        # make sure it's loaded and throw if not
-        if(-not (Get-Command "Invoke-MsBuild" -errorAction SilentlyContinue)){
-            throw ('Unable to install/load psbuild from [{0}]' -f $psbuildInstallUri)
-        }
-    }
-}
-
 # Taken from psake https://github.com/psake/psake
 
 <#  
@@ -48,17 +22,12 @@ function Exec
     }
 }
 
-if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
-
-#EnsurePsbuildInstalled
-
-exec { & dotnet restore }
-
-exec { & dotnet build "**/project.json" -c Release --no-dependencies }
-
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D5}" -f [convert]::ToInt32($revision, 10)
 
-exec { & dotnet test .\tests\Compose.Tests -c Release }
+if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
+exec { & dotnet restore }
+exec { & dotnet build "**/project.json" -c Release --no-dependencies }
+exec { & dotnet test .\tests\Compose.Tests -c Release }
 exec { & dotnet pack .\src\Compose -c Release -o .\artifacts --version-suffix=build$revision } 
